a2310eb649c181377b71531f9986f528c0b4f67b
guyvensonc adet <createwithcodes@gmail.com>
2024-05-10 11:31:46 -0400

    Initial Commit

---
+++ AirBnB/AirBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBApp.swift
+//  AirBnB
+//
+//  Created by Mac on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBnB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirBnB
+//
+//  Created by Mac on 5/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
